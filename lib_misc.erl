82efd05e649d41298ab62bfab0e0148abab85a20
Alan Briolat <alan@codescape.net>
2009-10-30 15:36:46 +0000

    Added spawn_n/2 for spawning multiple processes

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,6 +1,9 @@
 -module(lib_misc).
--export([min/2, max/2]).
+-export([min/2, max/2, spawn_n/2]).
 
 min(X, Y) -> lists:min([X, Y]).
 
 max(X, Y) -> lists:max([X, Y]).
+
+spawn_n(0, _Args) -> [];
+spawn_n(N, Args) -> [apply(spawn, Args) | spawn_n(N - 1, Args)].
